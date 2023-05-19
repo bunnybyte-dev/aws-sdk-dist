ab7f5220a634a6e75700c1e5df0405932ddc26d6
bballeysark <alleysark@bunnybyte.com>
2023-05-19 11:29:54 +0900

    @aws-sdk/s3-request-presigner 설치

--- bundle.js
+++ bundle.js
@@ -1,11 +1,13 @@
 import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 import { S3Client, ListObjectsCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
-import {fromCognitoIdentityPool} from "@aws-sdk/credential-provider-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 
 const Aws = {
   S3Client,
   ListObjectsCommand,
+  getSignedUrl,
   CognitoIdentityClient,
   fromCognitoIdentityPool,
 };
