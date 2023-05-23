7236a7def3bdfa8acc571ac0bdf98cfc08500dfa
bballeysark <alleysark@bunnybyte.com>
2023-05-23 12:08:51 +0900

    AmazonCognitoIdentity의 필요 모듈만 등록

--- bundle.js
+++ bundle.js
@@ -1,10 +1,17 @@
-import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
+import { CognitoUserPool, CognitoUser, CognitoUserAttribute, AuthenticationDetails } from "amazon-cognito-identity-js";
 import { S3Client, ListObjectsCommand, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 
 const Aws = {
+  // authentication service requires
+  CognitoUserPool,
+  CognitoUser,
+  CognitoUserAttribute,
+  AuthenticationDetails,
+
+  // asset service requires
   S3Client,
   ListObjectsCommand,
   GetObjectCommand,
@@ -14,7 +21,6 @@ const Aws = {
   fromCognitoIdentityPool,
 };
 
-if (typeof window !== "undefined") {
-  window.AmazonCognitoIdentity = AmazonCognitoIdentity;
+if (typeof window !== 'undefined') {
   window.Aws = Aws;
 }
